c6a3b68d81d94eb5a1f9aaae9651031a584751d1
Mustafa Anas <khmostafa4@gmail.com>
2019-11-05 23:14:49 -0500

    secure websocket connection

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const token = localStorage.getItem('token');
 
 // Create a WebSocket link :
 const link = new WebSocketLink({
-  uri: `ws://localhost:4000/graphql`,
+  uri: `wss://localhost:4000/graphql`,
   options: {
     reconnect: true,
   }
